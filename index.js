a265193eb451547240d396f1591f633eb5822d79
Gereon Czerny <gereon.czerny@yahoo.com>
2023-03-25 09:08:27 +0100

    fuckmocha

--- index.js
+++ index.js
@@ -1,17 +1,52 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.itmes.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    this.items.indexOf(pos);
+    if (pos > this.items.length){
+      throw new Error('OutOfBounds');
+    }
+  }
 
-  max() {}
+  max() {
+    getMaxOfArray(this.items);
+    if (this.items.length == 0){
+      throw new Error('EmptySortedList');
+    }
+  }
 
-  min() {}
+  min() {
+    getMinOfArray(this.items);
+    if (this.items.length == 0){
+      throw new Error('EmptySortedList');
+    }
+  }
 
-  sum() {}
+  sum() {
+    let sum = 0;
+    for (let i = 0; i < this.items.length; i++){
+      sum += this.items[i];
+    }
+    return sum
+  }
 
-  avg() {}
+  avg() {
+    let sum = 0;
+    let average = sum/this.items.length; 
+    for (let i = 0; i < this.items.length; i++){
+      sum += this.items[i];
+    }
+    return average
+  }
 }
 
 module.exports = SortedList;
